2a061a73ab10a4343fb44673551a4d7c2778a2cd
KayLotura2 <kaylotura@gmail.com>
2018-04-02 18:10:14 -0700

    notes on cards exercise

---
+++ cards.py
@@ -0,0 +1,4 @@
+# Please write a set of classes that represent a deck of cards.
+# Classes like “card” and “deck” might be appropriate - but it’s entirely up to you.  You should be able to “shuffle” the deck.  You should also be able to draw cards from the deck one at a time until the deck is empty.  Include tests.
+# Please track changes in Git and submit your code as a GitHub repository we can view.
+# This task shouldn't take more than two hours to complete and can be completed and returned as your schedule allows.
