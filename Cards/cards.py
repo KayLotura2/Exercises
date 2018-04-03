1512b67e5c782f4c83b56e54fc07f71c218dba2c
Hekate Nichols <Kaylotura@gmail.com>
2018-04-02 19:30:53 -0700

    testing a push

--- Cards/cards.py
+++ Cards/cards.py
@@ -2,3 +2,4 @@
 # Classes like “card” and “deck” might be appropriate - but it’s entirely up to you.  You should be able to “shuffle” the deck.  You should also be able to draw cards from the deck one at a time until the deck is empty.  Include tests.
 # Please track changes in Git and submit your code as a GitHub repository we can view.
 # This task shouldn't take more than two hours to complete and can be completed and returned as your schedule allows.
+# Test to configure git locally
